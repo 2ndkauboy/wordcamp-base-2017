c3cc042af7288021dd77ae1fdfbc0724e1efb6bd
2ndkauboy <bernhard@kau-boys.de>
2016-11-29 00:38:01 +0100

    Added sidebar-2 to secondary

--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,25 @@
 <?php
 /**
- * The sidebar containing the main widget area.
+ * The sidebar containing the main and secondary widget area.
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package WordCamp_Base_2017
  */
-
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
 ?>
 
 <aside id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+		<div id="secondary-sidebar" class="sidebar sidebar-1">
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+		<div id="secondary-sidebar" class="sidebar sidebar-2">
+			<?php dynamic_sidebar( 'sidebar-2' ); ?>
+		</div>
+	<?php endif; ?>
 </aside><!-- #secondary -->
+
+
